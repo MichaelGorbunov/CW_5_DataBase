b32e6774b600d2b5047aab197992dd0036a59a0f
Michael Gorbunov <mg0rby@yandex.ru>
2024-08-28 08:17:05 +0300

    наброски api, db utils

---
+++ src/api_hh.py
@@ -0,0 +1,53 @@
+import requests
+from typing import Any
+
+
+def get_employers_by_name(name: str, per_page: int = 20) -> list[dict[str, Any]]:
+    """
+    Функция для получения списка работодателей по имени с hh.ru.
+    :param name: Имя работодателя для поиска.
+    :param per_page: Количество работодателей на одной странице (по умолчанию 20).
+    :return: Список работодателей.
+    """
+    url = "https://api.hh.ru/employers"
+    params = {
+        "text": name,
+        "per_page": per_page,
+
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    employers_data = response.json()
+    print(employers_data.get("items", []))
+    return employers_data.get("items", [])
+
+
+
+
+def get_vacancies_by_employer(employer_id: int, per_page: int = 20) -> list[dict[str, Any]]:
+    """
+    Функция для получения списка вакансий работодателя с hh.ru.
+    :param employer_id: ID работодателя.
+    :param per_page: Количество вакансий на одной странице (по умолчанию 20).
+    :return: Список вакансий.
+    """
+    url = "https://api.hh.ru/vacancies"
+    params = {
+        "employer_id": employer_id,
+        "per_page": per_page,
+        "area": 113,  # Россия
+        "only_with_salary": True,  # Указана зарплата
+
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    vacancies_data = response.json()
+
+    return vacancies_data.get("items", [])
+
+if __name__ == '__main__':
+    # main()
+    # get_employers_by_name('МТС',100)
+    get_vacancies_by_employer(3529,100)
---
+++ src/db_util.py
@@ -0,0 +1,54 @@
+import psycopg2
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+db_config = {
+    'user': os.getenv('POSTGRES_USER'),
+    'password': os.getenv('POSTGRES_PASSWORD'),
+    'host': os.getenv('POSTGRES_HOST'),
+    'port': os.getenv('POSTGRES_PORT'),
+    'dbname': os.getenv('POSTGRES_DB')
+}
+def create_tables() -> None:
+    conn = psycopg2.connect(**db_config)
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS companies (
+                company_id SERIAL PRIMARY KEY,
+                company_name VARCHAR(255) NOT NULL,
+                company_url TEXT,
+                open_vacancies INTEGER
+            );
+        """)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS vacancies (
+                vacancy_id SERIAL PRIMARY KEY,
+                company_id INTEGER REFERENCES companies(company_id),
+                title VARCHAR(255) NOT NULL,
+                salary_from INTEGER,
+                salary_to INTEGER,
+                vacancy_url TEXT
+            );
+        """)
+    conn.commit()
+    conn.close()
+def create_db():
+    conn = psycopg2.connect(database='postgres', user=os.getenv('POSTGRES_USER'),
+                            password=os.getenv('POSTGRES_PASSWORD'),
+                            host=os.getenv('POSTGRES_HOST'),
+                            port=os.getenv('POSTGRES_PORT'))
+    cursor = conn.cursor()
+    conn.autocommit = True
+
+    sql_qr = f"CREATE DATABASE {db_config['dbname']}"
+    cursor.execute(sql_qr)
+    print("База данных успешно создана")
+    cursor.close()
+    conn.close()
+
+
+create_tables()
