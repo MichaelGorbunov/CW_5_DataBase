856a37d2d2157867d83013cc07766f317d82c133
Michael Gorbunov <mg0rby@yandex.ru>
2024-08-29 10:38:05 +0300

    get_vacancies_with_keyword

--- src/db_manager.py
+++ src/db_manager.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import Any, List, Tuple
+import os
+import psycopg2
+from dotenv import load_dotenv
+
 
 
 class DBManagerABC(ABC):
@@ -25,3 +29,63 @@ class DBManagerABC(ABC):
     def get_vacancies_with_keyword(self, keyword: str) -> List[Tuple[Any, ...]]:
         pass
 
+
+
+
+
+
+class DBManager(DBManagerABC):
+    def __init__(self):
+        load_dotenv()
+        db_config = {
+            "user": os.getenv("POSTGRES_USER"),
+            "password": os.getenv("POSTGRES_PASSWORD"),
+            "host": os.getenv("POSTGRES_HOST"),
+            "port": os.getenv("POSTGRES_PORT"),
+            "dbname": os.getenv("POSTGRES_DB"),
+        }
+
+        self.connection = psycopg2.connect(**db_config)
+        self.connection.autocommit = True
+        if self.connection:
+            print("база открыта")
+
+    def __del__(self):
+        if self.connection:
+            self.connection.close()
+            print("соединения закрыто")
+
+    def get_companies_and_vacancies_count(self):
+        pass
+
+    def get_all_vacancies(self):
+        pass
+
+    def get_avg_salary(self):
+        pass
+
+    def get_vacancies_with_higher_salary(self):
+        pass
+
+    def get_vacancies_with_keyword(self, keyword:str):
+        with self.connection.cursor() as cur:
+            search_pattern = f"%{keyword.lower()}%"
+            cur.execute(f"""
+                SELECT companies.company_name, vacancies.vacan_title,
+                vacancies.salary_from,vacancies.vacancy_url
+                FROM vacancies
+                JOIN companies ON vacancies.company_id = companies.company_id
+                WHERE LOWER(vacancies.vacan_title) LIKE '{search_pattern}' OR  
+                LOWER(vacancies.vacan_req) LIKE '{search_pattern}' OR
+                LOWER(vacancies.vacan_resp) LIKE '{search_pattern}'
+                ;
+            """)
+            result = cur.fetchall()
+        return result
+
+
+if __name__ == "__main__":
+    db_manager = DBManager()
+    vacancies = db_manager.get_vacancies_with_keyword('кран')
+    for item in vacancies:
+        print("'ыаыва':", item)
